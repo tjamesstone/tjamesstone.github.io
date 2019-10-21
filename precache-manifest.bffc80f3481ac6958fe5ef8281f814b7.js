828581a10c94fc96478c0be26743ef9be849b5b6
Tate Stone <tatejamesstone@gmail.com>
2019-10-21 15:45:20 -0600

    Updates

---
+++ precache-manifest.bffc80f3481ac6958fe5ef8281f814b7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702a31a7019efe50b2316f9aa782b4a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b3f23ed02eef062381",
+    "url": "/static/css/main.4b51fb25.chunk.css"
+  },
+  {
+    "revision": "6e83d9202a1f645b11b1",
+    "url": "/static/js/2.e5d195f7.chunk.js"
+  },
+  {
+    "revision": "84b3f23ed02eef062381",
+    "url": "/static/js/main.139d5e83.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "1a9a9c54fadbdd845398dba0ecb5c26d",
+    "url": "/static/media/1.1a9a9c54.png"
+  },
+  {
+    "revision": "95e85c67c0f1e1b9a8736389126d994e",
+    "url": "/static/media/3.95e85c67.png"
+  },
+  {
+    "revision": "88b4a9bc67aa65b904c826994d791470",
+    "url": "/static/media/4.88b4a9bc.png"
+  },
+  {
+    "revision": "a08c615948356d32649a535fdab2bf93",
+    "url": "/static/media/5.a08c6159.png"
+  },
+  {
+    "revision": "27d1b96481625f9aafea622fae826ca6",
+    "url": "/static/media/7.27d1b964.png"
+  },
+  {
+    "revision": "4cf00bda865a0838149ed7fcc67c235a",
+    "url": "/static/media/byu2.4cf00bda.png"
+  },
+  {
+    "revision": "d04c9b6c7d3389c9baacd855d2a13b8a",
+    "url": "/static/media/createacontract.d04c9b6c.png"
+  },
+  {
+    "revision": "17a6652f345bc5ea2db2d4fba0c95a84",
+    "url": "/static/media/img_326384.17a6652f.png"
+  },
+  {
+    "revision": "425c0312ff1a933f3faa7539bef999cf",
+    "url": "/static/media/linkedin.425c0312.png"
+  },
+  {
+    "revision": "74c362907475d0d7f0d59d0fb662135c",
+    "url": "/static/media/sass.74c36290.png"
+  },
+  {
+    "revision": "70f54f4b2a8f7a6c30a2e1a3f847fc10",
+    "url": "/static/media/sf2.70f54f4b.png"
+  },
+  {
+    "revision": "3abb83d9f82697985b162d4b859bb4af",
+    "url": "/static/media/z.3abb83d9.png"
+  }
+]);
